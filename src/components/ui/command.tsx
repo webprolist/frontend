54b83edd4c1798e0cfb308b1e2a70af034af8622
SANGYOON PARK <parkyoon97@gmail.com>
2024-11-24 02:37:35 +0900

    FEAT: shadcn UI 설정
    add
    - alert-dialog
    - alert
    - aspect-ratio
    - avatar
    - badge
    - button
    - card
    - collapsible
    - command
    - dialog
    - dropdown-menu
    - label
    - navigation-menu
    - progress
    - scroll-area
    - sonner
    - textarea

---
+++ src/components/ui/command.tsx
@@ -0,0 +1,46 @@
+import * as React from "react"
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons"
+import { Command as CommandPrimitive } from "cmdk"
+
+import { cn } from "@/lib/utils"
+
+const Command = React.forwardRef<
+  React.ElementRef<typeof CommandPrimitive>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive>
+>(({ className, ...props }, ref) => (
+  <CommandPrimitive
+    ref={ref}
+    className={cn(
+      "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground",
+      className
+    )}
+    {...props}
+  />
+))
+Command.displayName = CommandPrimitive.displayName
+
+
+const CommandInput = React.forwardRef<
+  React.ElementRef<typeof CommandPrimitive.Input>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
+>(({ className, ...props }, ref) => (
+  <div className="flex items-center border rounded-lg px-3" cmdk-input-wrapper="">
+    <MagnifyingGlassIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+    <CommandPrimitive.Input
+      ref={ref}
+      className={cn(
+        "flex h-9 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      {...props}
+    />
+  </div>
+))
+
+CommandInput.displayName = CommandPrimitive.Input.displayName
+
+
+export {
+  Command,
+  CommandInput,
+}
